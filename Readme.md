c66d90920d3fa1f731f2d68ebd0d516eee49d9b3
Rison Simon <risonsimon@gmail.com>
2016-12-15 17:01:54 +0530

    sample webvr app in Viewport

---
+++ Readme.md
@@ -0,0 +1,9 @@
+## Viewport - Build VR apps in 5 minutes
+
+[Viewport](https://viewportvr.co) is a cross platform, scalable VR API to create virtual reality (VR) applications. Viewport makes it simple to publish multiple content in VR.
+
+## How to build the VR app
+
+To create VR apps using Viewport, first register at [Viewport docs](http://docs.viewportvr.co). Once you register, you'll receive the details to get started with Viewport.
+
+This sample shows how easy it is to built a VR app.
---
+++ viewport.js
@@ -0,0 +1,27 @@
+var file = document.getElementById('file');
+file.addEventListener('change', function(event) {
+  var files = file.files;
+  for(var i = 0; i < files.length ; i++) {
+    var formData = new FormData;
+    formData.append('name', files[i].name);
+    formData.append('file', files[i]);
+
+    var post = new XMLHttpRequest();
+    var apiUrl = 'https://api.viewport.teliportme.com/contents/?sdk=vjs-0.1';
+    post.open("POST", apiUrl, true);
+    post.onload = function() {
+      showEmbed(this.responseText);
+    };
+    post.send(formData);
+  }
+});
+
+function showEmbed(response) {
+  if (response) {
+    var viewportId = JSON.parse(response).response.contentId;
+    var embed = document.createElement('iframe');
+    embed.style = "width:100%;height: 500px;"
+    embed.src = "https://embed.viewportvr.co/embed/" + viewportId;
+    document.getElementById('embed').appendChild(embed);
+  }
+}
